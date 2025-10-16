d57d4d2fb01a22a6b6e33658a969c5d55979ca08
bryanmsombra27 <bryanjm96@gmail.com>
2025-10-15 18:32:40 -0600

    adding token verification error

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -38,6 +38,7 @@ export class AuthService {
 
       return decodeToken;
     } catch (error) {
+      this.cacheService.deleteFromCache(token);
       throw new UnauthorizedException('Token invalido');
     }
   }
